0e329f983fefe7014e1c4f5875971db5f229b2c6
Arjun Shrestha <arjun@namespace.jp>
2023-10-06 13:11:21 +0545

    configured react-native-config

--- template/src/screens/HomeScreen/index.tsx
+++ template/src/screens/HomeScreen/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text} from 'react-native';
+import Config from 'react-native-config';
 
 export const HomeScreen = () => {
-  return <Text>Welcome Home</Text>;
+  return <Text>Welcome Home {Config.ENV_TEST}</Text>;
 };
